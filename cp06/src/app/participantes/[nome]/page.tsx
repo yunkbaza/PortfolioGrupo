b2157cf117afff2b6bdac6334b933652180b944c
jubshereman <lancrazy02@gmail.com>
2024-10-22 11:33:46 -0300

    fazendo a componentização

--- cp06/src/app/participantes/[nome]/page.tsx
+++ cp06/src/app/participantes/[nome]/page.tsx
@@ -1,5 +1,12 @@
+import Cabecalho from "@/components/Cabecalho/Cabecalho";
+import Menu from "@/components/Menu/Menu";
+import Rodape from "@/components/Rodape/Rodape";
+
 export default function ParticipantPage({ params }: { params: { nome: string } }) {
-    <div>
-        <h1>vou me matar </h1>
-    </div>
+
+    return (
+        <Cabecalho/>
+        <Menu/>
+        <Rodape/>
+    );
 }
